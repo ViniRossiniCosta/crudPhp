624251d873ef471c36323340a9f88b1cd094de7e
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-15 11:59:09 -0300

    processa.php refeito

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <title>CRUD - cadastrar</title>
 </head>
 <body>
-    
+    <h1>Cadastrar Usuario</h1>
+    <form action="POST" action="processa.php">
+        <label for="">Nome: </label>
+        <input type="text" name="" id="" placeholder="digite seu nome">
+        <label for="">E-mail: </label>
+        <input type="email" name="" id="" placeholder="digite seu email">
+
+        <input type="submit" value="cadastrar">
+    </form>
 </body>
 </html>
---
+++ processa.php
@@ -0,0 +1,8 @@
+<?php 
+
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+
+
+?>
