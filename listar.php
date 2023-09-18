c8c23c326ebf031188e1764fd5ca96a02c582a15
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-18 11:59:31 -0300

    kdsakd

--- listar.php
+++ listar.php
@@ -1,6 +1,6 @@
-<?php 
-    session_start();
-    include_once("conexao.php");
+<?php
+session_start();
+include_once("conexao.php");
 ?>
 
 <!DOCTYPE html>
@@ -15,24 +15,54 @@
 <body>
     <h1>Lista de usuarios</h1>
 
-    <?php 
-        if(isset($_SESSION['msg'])){
-            echo $_SESSION['msg'];
-            echo $_SESSION['msg'];
+    <?php
+    if (isset($_SESSION['msg'])) {
+        echo $_SESSION['msg'];
+        echo $_SESSION['msg'];
+    }
+
+    $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);
+
+    $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
+
+    $qnt_result_pg = 2;
+
+    $inicio = ($qnt_result_pg * $pagina) - $qnt_result_pg;
+
+    $result_usuario = "SELECT * FROM usuarios LIMIT $inicio, $qnt_result_pg";
+    $resultado_usuario = mysqli_query($conn, $result_usuario);
+    while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+        echo "ID: " . $row_usuario['id'] . "<br>";
+        echo "Nome: " . $row_usuario['nome'] . "<br>";
+        echo "E-mail: " . $row_usuario['email'] . "<br>";
+    }
+
+    $result_pg = "SELECT COUNT(id) AS num_result FROM usuarios";
+    $resultado_pg = mysqli_query($conn, $resultado_pg);
+    $row_pg = mysqli_fetch_assoc($resultado_pg);
+    echo $row_pg['num_result'];
+
+    $quantidade_pg = ceil($row_pg['num_result'] / $qnt_result_pg);
+
+    $max_links = 2;
+
+    echo "<a href='listar.php?pagina=1'> Primeira </a>";
+
+    for ($pag_ant = $pagina - $max_links; $pag_ant <= $pagina - 1; $pag_ant++) {
+        if($pag_ant >= 1) {
+            echo "<a herf='listar.php?pagina=$pag_ant'> $pag_ant</a>";
         }
+    }
 
-        $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);
+    echo "$pagina";
 
-        $pagina = (!empty($pagina_atual)) ? $pagina_atual: 1;
+    echo "<a href='listar.php?pagina=$quantidade_pg'> Ultima </a>";
 
-        $result_usuario = "SELECT * FROM usuarios";
-        $resultado_usuario = mysqli_query($conn,$result_usuario);
-        while ($row_usuario = mysqli_fetch_assoc($resultado_usuario)){
-            echo "ID: " . $row_usuario['id'] . "<br>";
-            echo "Nome: " . $row_usuario['nome'] . "<br>";
-            echo "E-mail: " . $row_usuario['email'] . "<br>";
+    for($pag_dps = $pagina + 1; $pag_dps <= $pagina + $max_links; $pag_dps ++) {
+        if($pag_dps <= $quantidade_pg){
+            echo "<a herf='listar.php?pagina=$pag_dps'>$pag_dps</a>";
         }
-
+    }
 
     ?>
 
